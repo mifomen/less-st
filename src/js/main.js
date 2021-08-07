6eefa1db3fee06bfbd1af652d4bcb343f38389d5
Ilya <mifomen@yandex.ru>
2021-08-08 00:26:15 +0300

    add localstorage

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,26 @@
-console.log('work')
+'use strict'
+
+let WidgetButton = document.querySelector('.js-close-widget')
+let Widget = document.querySelector('.widget')
+
+WidgetButton.addEventListener('click', function (evt) {
+  Widget.classList.toggle('vh')
+  localStorage.setItem('WidgetClosed', 'true');
+})
+
+let deleteWidget = function () {
+  localStorage.removeItem('WidgetClosed')
+  return true
+  // return console.log('removeItemWidgetClosed')
+}
+
+// console.log( localStorage.getItem('WidgetClosed') ); // 1
+
+function readyWidget() {
+  if (!localStorage.getItem('WidgetClosed')) {
+    return Widget.classList.remove('vh')
+  }  else {
+   return Widget.classList.add('vh')
+ }
+}
+document.addEventListener("DOMContentLoaded", readyWidget);
