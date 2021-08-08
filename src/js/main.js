da083d0f80f8ad550d413f463914be0035b7a9cb
Ilya Koshelev <mifomen@yandex.ru>
2021-08-08 13:55:47 +0300

    fix??

--- src/js/main.js
+++ src/js/main.js
@@ -1,37 +1,27 @@
-// console.log('work')
-
-let div1 = document.querySelector('#div1');
-let div2 = document.querySelector('#div2');
-
-// console.log(div1.innerHTML)
-
-let start = function (t1,t2) {
-  t1.innerHTML = t2.innerHTML;
-  // console.log('start')
+let start = function () {
+  let div1 = document.querySelector('#div2');
+  let div2 = document.querySelector('#div1');
+  return div1.innerHTML = div2.innerHTML;
 }
-
-start(div2, div1);
-
-// console.log(div1.innerHTML)
-// console.log(div2.innerHTML)
+document.addEventListener("DOMContentLoaded", () => {
+  start();
+});
 
 let Div1CloseBtn = document.querySelector('.div1__btn')
+console.log(Div1CloseBtn)
 
-// console.log(Div1CloseBtn)
-
+// for (button of Div1CloseBtn)  {
 Div1CloseBtn.addEventListener('click', function() {
-  div1.classList.add('vh')
-  Div1CloseBtn.classList.add('vh')
+  document.querySelector('#div2').classList.add('vh')
+  // button.classList.add('vh')
   console.log('close')
 })
+// }
 
-
-
- document.addEventListener("DOMContentLoaded", () => {
-    // alert("DOM готов!");
-    div1.classList.remove('vh')
-  });
+console.log(Div1CloseBtn)
 
 
 localStorage.setItem('OpenMsg', 'true mif');
 console.log( localStorage.getItem('OpenMsg') ); // 1
+
+// document.addEventListener("DOMContentLoaded", start);
