f19d6769094dba9b88a05c7622f67ae999374224
Ilya Koshelev <mifomen@yandex.ru>
2021-08-10 16:47:28 +0300

    clear fix ??? may be no

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // console.log('work')
 
 let start = function (t1,t2) {
@@ -34,32 +33,7 @@ document.addEventListener("DOMContentLoaded", () => {
     // alert("DOM готов!");
     div2.classList.remove('vh')
   });
-=======
-let start = function () {
-  let div1 = document.querySelector('#div2');
-  let div2 = document.querySelector('#div1');
-  return div1.innerHTML = div2.innerHTML;
-}
-document.addEventListener("DOMContentLoaded", () => {
-  start();
-});
-
-let Div1CloseBtn = document.querySelector('.div1__btn')
-console.log(Div1CloseBtn)
-
-// for (button of Div1CloseBtn)  {
-Div1CloseBtn.addEventListener('click', function() {
-  document.querySelector('#div2').classList.add('vh')
-  // button.classList.add('vh')
-  console.log('close')
-})
-// }
-
-console.log(Div1CloseBtn)
->>>>>>> da083d0f80f8ad550d413f463914be0035b7a9cb
 
 
 localStorage.setItem('OpenMsg', 'true mif');
 console.log( localStorage.getItem('OpenMsg') ); // 1
-
-// document.addEventListener("DOMContentLoaded", start);
