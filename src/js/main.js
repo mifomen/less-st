c2fcaabd411a7029780b0556ae440d9951e2435c
Ilya Koshelev <mifomen@yandex.ru>
2021-08-11 15:41:54 +0300

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,33 +1,33 @@
-
 let start = function (t1,t2) {
   let div1 = document.querySelector(t1);
   let div2 = document.querySelector(t2);
   div2.innerHTML = div1.innerHTML;
-  console.log(t1 + ' in ' + t2)
 }
-
-// start('#div2', '#div1');
 
 document.addEventListener("DOMContentLoaded", start('#div2', '#div1'));
 
-let closeBtns = document.querySelectorAll('.div1__btn')
+let statusOfHidden = function (evt) {
+  if (localStorage.getItem('headerMsgClosed')==='true') {
+    return true;
+  } else {
 
-
-for (btn of closeBtns) {
-  btn.addEventListener('click', function() {
-    if (this.parentNode.getAttribute('data-hidden') ==='true')
-    {
-      this.parentNode.classList.add('vh')
-      this.parentNode.style.cssText = "display: none;"
+    if ( document.querySelector('#div2').getAttribute('data-hidden') ==='false') {
+      document.querySelector('#div1').classList.remove('vh')
+      document.querySelector('#div1').style.cssText=""
     }
-    // console.log('close ' + this.parentNode.id)
-  })
+    if ( document.querySelector('#div2').getAttribute('data-btn') ==='true') {
+
+      console.log('headerMsgClosed= ' + localStorage.getItem('headerMsgClosed'))
+      let closeBtns = document.querySelectorAll('.div1__btn');
+
+      for (var closeBtn of closeBtns) {
+        closeBtn.addEventListener('click', function() {
+          localStorage.setItem('headerMsgClosed', 'true')
+          this.parentNode.classList.add('vh')
+          this.parentNode.style.cssText = "display: none;"
+        })
+      }
+    }
+  }
 }
-
-
-document.addEventListener("DOMContentLoaded", () => {
-    document.querySelector('#div1').classList.remove('vh')
-  });
-
-localStorage.setItem('OpenMsg', 'true mif');
-console.log( localStorage.getItem('OpenMsg') ); // 1
+document.addEventListener("DOMContentLoaded", statusOfHidden)
