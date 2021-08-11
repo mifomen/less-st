77dc94b91da7a7ce34c4e4997eeb991e4376568a
Ilya Koshelev <mifomen@yandex.ru>
2021-08-11 13:07:29 +0300

    fitx.... again

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,3 @@
-// console.log('work')
 
 let start = function (t1,t2) {
   let div1 = document.querySelector(t1);
@@ -7,33 +6,28 @@ let start = function (t1,t2) {
   console.log(t1 + ' in ' + t2)
 }
 
-console.log(document.querySelector('#div1'))
-console.log(document.querySelector('#div2'))
-console.log(document.querySelector('#div3'))
+// start('#div2', '#div1');
 
-start('#div1', '#div2');
-// start('#div1', '#div3')
-// start('#div1', '#div3');
-// console.log(div1.innerHTML)
-// console.log(div2.innerHTML)
+document.addEventListener("DOMContentLoaded", start('#div2', '#div1'));
 
 let closeBtns = document.querySelectorAll('.div1__btn')
 
-// console.log(closeBtns)
+
 for (btn of closeBtns) {
   btn.addEventListener('click', function() {
-    // this.parentElement.classList.add('vh')
-    this.parentNode.classList.add('vh')
-    console.log('close ' + this.parentNode.id)
+    if (this.parentNode.getAttribute('data-hidden') ==='true')
+    {
+      this.parentNode.classList.add('vh')
+      this.parentNode.style.cssText = "display: none;"
+    }
+    // console.log('close ' + this.parentNode.id)
   })
 }
 
 
 document.addEventListener("DOMContentLoaded", () => {
-    // alert("DOM готов!");
-    div2.classList.remove('vh')
+    document.querySelector('#div1').classList.remove('vh')
   });
-
 
 localStorage.setItem('OpenMsg', 'true mif');
 console.log( localStorage.getItem('OpenMsg') ); // 1
